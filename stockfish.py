9d77df9dcd64df10ddc770b76f8d5586dfe60419
Simone <sbattistini98@gmail.com>
2026-03-16 18:01:56 +0100

    added games + full loop with accuracies + accuracy function moved in the module

--- stockfish.py
+++ stockfish.py
@@ -1,9 +1,10 @@
 import chess
 import chess.engine
 import chess.pgn
+import numpy as np
 
 from utils.winPercentage import get_line_win_percentage
-from utils.accuracy import get_move_accuracy
+from utils.accuracy import get_move_accuracy, get_player_accuracy, calc_weights
 
 
 class StockfishWrapper:
@@ -17,32 +18,67 @@ class StockfishWrapper:
 
 engine = chess.engine.SimpleEngine.popen_uci("engine/stockfish_16_1")
 depth = 22
-with open("games/game.pgn") as f:
+with open("games/game2.pgn") as f:
     game = chess.pgn.read_game(f)
 
 board = game.board()
 
 moves_win_percentage = []
+moves_accuracy = []
 deltas = []
 w_acc = []
 b_acc = []
+
 for index, move in enumerate(game.mainline_moves()):
     player = board.turn
     info_before = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=2)
     win_before = get_line_win_percentage(info_before[0], player)
     moves_win_percentage.append(win_before)
     board.push(move)
+    if board.is_checkmate():
+        win_after = 100.0 if player == chess.WHITE else 0.0
+        delta = win_after - win_before
+        accuracy = get_move_accuracy(delta, player)
+        if player == chess.WHITE:
+            w_acc.append(accuracy)
+        else:
+            b_acc.append(accuracy)
+        print(f"{(index//2)+1}. ({'B' if player==chess.WHITE else 'N'}) "
+              f"Before {win_before:.1f}, After {win_after:.1f}, Δ {delta:.1f}, acc {accuracy:.1f}")
+        print("Partita finita dopo questa mossa (checkmate)")
+        break
+    elif board.is_stalemate():
+        win_after = 50.0  # patta
+        delta = win_after - win_before
+        accuracy = get_move_accuracy(delta, player)
+        if player == chess.WHITE:
+            w_acc.append(accuracy)
+        else:
+            b_acc.append(accuracy)
+        print(f"{(index//2)+1}. "
+              f"Before {win_before:.1f}, After {win_after:.1f}, Δ {delta:.1f}, acc {accuracy:.1f}")
+        print("Partita finita dopo questa mossa (stalemate)")
+        break
     info_after = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=2)
     win_after = get_line_win_percentage(info_after[0], player)
     delta = win_after - win_before
     accuracy = get_move_accuracy(delta, player)
+    moves_accuracy.append(accuracy)
     if player == chess.WHITE:
-        print(
-            f"{(index // 2) + 1}. Before {win_before:.1f}, After {win_after:.1f}, Δ {delta:.1f}, acc {accuracy:.1f}",end=' ')
         w_acc.append(accuracy)
+        print(f"{(index//2)+1}. Before {win_before:.1f}, After {win_after:.1f}, Δ {delta:.1f}", end=' ')
     else:
-        print(
-            f"Before {win_before:.1f}, After {win_after:.1f}, Δ {delta:.1f}, acc {accuracy:.1f}")
         b_acc.append(accuracy)
+        print(f"Before {win_before:.1f}, After {win_after:.1f}, Δ {delta:.1f}, acc {accuracy:.1f}")
 
 engine.close()
+
+# calcolo pesi per bianco e nero separatamente
+w_weights = calc_weights(w_acc)
+b_weights = calc_weights(b_acc)
+
+white_accuracy = get_player_accuracy(w_acc, w_weights)
+black_accuracy = get_player_accuracy(b_acc, b_weights)
+
+print("White Accuracy:", white_accuracy)
+print("Black Accuracy:", black_accuracy)
--- utils/accuracy.py
+++ utils/accuracy.py
@@ -1,6 +1,38 @@
 import chess
-
 import math
+import numpy as np
+
+def get_player_accuracy(player_acc, player_weights):
+    w_mean = weighted_mean(player_acc, player_weights)
+    h_mean = harmonic_mean(player_acc)
+    return (w_mean + h_mean) / 2
+
+def calc_weights(moves_acc):
+    window_size = min(8, max(2, round(len(moves_acc)/10)))
+    half_window = round(window_size / 2)
+
+    weights = []
+    for i in range(len(moves_acc)):
+        start_idx = max(0, i - half_window)
+        end_idx = min(len(moves_acc), i + half_window)
+        window = moves_acc[start_idx:end_idx]
+        std = np.std(window)
+        weight = min(12, round(std*2)/2)
+        weights.append(weight)
+    return weights
+
+def weighted_mean(values, weights):
+    values = np.array(values)
+    weights = np.array(weights)
+    if len(values) == 0:
+        return 0.0
+    return np.sum(values * weights) / np.sum(weights)
+
+def harmonic_mean(values):
+    values = np.array(values)
+    if len(values) == 0:
+        return 0.0
+    return len(values) / np.sum(1.0/np.maximum(values, 10))  # replica getHarmonicMean
 
 def get_move_accuracy(delta: float, turn: chess.Color) -> float:
     if turn == chess.WHITE:
